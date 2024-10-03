9e7f02db690ff43b6e882428a0cd1182e0c358af
Philip Viernes <pviernes@crescendocollective.com>
2024-10-03 12:48:52 +0800

    First commit

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 import Typed from './typed.js';
-
 const typed = new Typed('.elements', {
   strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
   typeSpeed: 50,
 });
+const outer = document.querySelector('.outer-container');
+const body = document.querySelector('body')
+body.addEventListener('mouseover', event=>{
+    event.target.outer
+})
