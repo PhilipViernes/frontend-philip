69b5b3bca22fd9d5bf0c82467c3f59c223b2f38b
iamdrev <pviernes@crescendocollective.com>
2024-10-03 18:01:09 +0800

    Co-authored-by: iamdrev <viernes011599@gmail.com>

--- js.js
+++ js.js
@@ -1,10 +1,1 @@
-import Typed from './typed.js';
-const typed = new Typed('.elements', {
-  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  typeSpeed: 50,
-});
-const outer = document.querySelector('.outer-container');
-const body = document.querySelector('body')
-body.addEventListener('mouseover', event=>{
-    event.target.outer
-})
+
